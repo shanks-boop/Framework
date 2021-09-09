0184396fe0eb27fa1069217ffb0999772bb571d2
shanks-boop <73789710+shanks-boop@users.noreply.github.com>
2021-09-09 17:35:00 -0400

    Update GitHubDemo.java

--- GitHubDemo/src/test/java/com/git/hub/GitHubDemo.java
+++ GitHubDemo/src/test/java/com/git/hub/GitHubDemo.java
@@ -1,17 +1,22 @@
 package com.git.hub;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
 
 public class GitHubDemo {
 	
 	
 	
 	@Test
-	public void githubDemo () {
+	public void githubDemo () throws InterruptedException {
 		
 		
 		System.setProperty("webdriver.chrome.driver",
@@ -19,13 +24,54 @@ public class GitHubDemo {
 		
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
 		
-		driver.findElement(By.name("q")).sendKeys("test" + Keys.ENTER);
+		driver.get("https://www.webstaurantstore.com/"); //navigate to web store
 		
-		//test change
-		//test change2
+		driver.findElement(By.id("searchval")).sendKeys("stainless work table" + Keys.ENTER); //search for item
 		
+		Thread.sleep(4000); //ideally should use a proper wait command .. like wait for for element to be visible or clickable (using a short cut due to time constraints .. sorry!)
+		
+		List<WebElement> elementName = driver.findElements(By.xpath("//a[@data-testid='itemDescription']"));
+		
+		System.out.println(elementName.size());
+		
+		int i = 1;
+		
+		//check all the 60 links if they contain the word Table.  There should be a number 60 in the console for the LAST line
+		for (WebElement webElement : elementName) {
+            String name = webElement.getText();
+            //System.out.println(name);
+            
+            if (name.contains("Table"))
+              System.out.println("The list contains Table .. whoohooo" + " " + i);
+          else
+              System.out.println("UH OH");
+            
+            i++;
+            
+        }
+		
+		Thread.sleep(4000);  
+		
+		WebElement element = driver.findElement(By.xpath("(//a[@data-testid='itemDescription'])[60]"));
+		
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element); //scroll the last item into view to click on it
+		
+		driver.findElement(By.xpath("(//a[@data-testid='itemDescription'])[60]")).click(); //click on the last item in the list
+		
+		
+		Thread.sleep(4000); 
+		
+		driver.findElement(By.id("buyButton")).click(); //click on buy button to add item to cart
+		
+		Thread.sleep(4000);
+		
+		driver.findElement(By.xpath("//a[@data-testid='cart-nav-link']")).click(); //click on cart to see items
+		
+		Thread.sleep(4000);
+		
+		driver.findElement(By.cssSelector("svg.itemDelete__icon")).click(); //click to empty cart
 		
 		
 	}
